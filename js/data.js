cf9331ef1f8e5e059dc1cb76f5cced8febfe2740
xiongod <xiongod@outlook.com>
2024-05-21 11:42:51 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,7 +5,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 106
+        "chapter": 106,
+        "id": "674adad9155540a098c3685510ce5462"
     },
     {
         "title": "107.【试听】《哈利·波特》第三部 第2集 猫头鹰传书2.m4a",
@@ -13,7 +14,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 107
+        "chapter": 107,
+        "id": "b3264b5fb66a41b9b6f4c73ae2b5944b"
     },
     {
         "title": "108.【试听】《哈利·波特》第三部 第3集 玛姬姑妈的大错误1.m4a",
@@ -21,7 +23,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 108
+        "chapter": 108,
+        "id": "912974d702a74dc3a0f8f721176745a6"
     },
     {
         "title": "109.《哈利·波特》第三部 第4集 玛姬姑妈的大错误2.m4a",
@@ -29,7 +32,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 109
+        "chapter": 109,
+        "id": "3b4d44a508774f3699fc25cc98e25b23"
     },
     {
         "title": "110.《哈利·波特》第三部 第5集 骑士公共汽车1.m4a",
@@ -37,7 +41,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 110
+        "chapter": 110,
+        "id": "1c40b43374d24f23a3702aae86958159"
     },
     {
         "title": "111.《哈利·波特》第三部 第6集 骑士公共汽车2.m4a",
@@ -45,7 +50,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 111
+        "chapter": 111,
+        "id": "7a01ef752e27477c8e6fb274c2137f84"
     },
     {
         "title": "112.《哈利·波特》第三部 第7集 破釜酒吧1.m4a",
@@ -53,7 +59,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 112
+        "chapter": 112,
+        "id": "7caac464e6694bf8be0afacdc5a81aab"
     },
     {
         "title": "113.《哈利·波特》第三部 第8集 破釜酒吧2.m4a",
@@ -61,7 +68,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 113
+        "chapter": 113,
+        "id": "3fb1db98210249c4821c1c8aba92fe89"
     },
     {
         "title": "114.《哈利·波特》第三部 第9集 破釜酒吧3.m4a",
@@ -69,7 +77,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 114
+        "chapter": 114,
+        "id": "7f092f0b508e49a88699e5e43ca0b811"
     },
     {
         "title": "115.《哈利·波特》第三部 第10集 摄魂怪1.m4a",
@@ -77,7 +86,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 115
+        "chapter": 115,
+        "id": "2151e353c6e94215bc0e43555d557b86"
     },
     {
         "title": "116.《哈利·波特》第三部 第11集 摄魂怪2.m4a",
@@ -85,7 +95,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 116
+        "chapter": 116,
+        "id": "dccc60f7bb6143f890f39dcd00c281a6"
     },
     {
         "title": "117.《哈利·波特》第三部 第12集 摄魂怪3.m4a",
@@ -93,7 +104,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 117
+        "chapter": 117,
+        "id": "9205c0d576094732aa8884471afdd408"
     },
     {
         "title": "118.《哈利·波特》第三部 第13集 摄魂怪4.m4a",
@@ -101,7 +113,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 118
+        "chapter": 118,
+        "id": "5262e26a5f1f452793e68194d6edf67b"
     },
     {
         "title": "119.《哈利·波特》第三部 第14集 鹰爪和茶叶1.m4a",
@@ -109,7 +122,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 119
+        "chapter": 119,
+        "id": "fcc3412e45024eb784833d14ab7f03eb"
     },
     {
         "title": "120.《哈利·波特》第三部 第15集 鹰爪和茶叶2.m4a",
@@ -117,7 +131,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 120
+        "chapter": 120,
+        "id": "11e6294878144661b677cd1d0cf81f9f"
     },
     {
         "title": "121.《哈利·波特》第三部 第16集 鹰爪和茶叶3.m4a",
@@ -125,7 +140,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 121
+        "chapter": 121,
+        "id": "13a69f88a27e4838b2790a82e56488a3"
     },
     {
         "title": "122.《哈利·波特》第三部 第17集 鹰爪和茶叶4.m4a",
@@ -133,7 +149,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 122
+        "chapter": 122,
+        "id": "d80b5ce9a9e94a14b0079b45ddf4298b"
     },
     {
         "title": "123.《哈利·波特》第三部 第18集 鹰爪和茶叶5.m4a",
@@ -141,7 +158,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 123
+        "chapter": 123,
+        "id": "8b897824d3654da2af48648f599de2c4"
     },
     {
         "title": "124.《哈利·波特》第三部 第19集 衣柜里的博格特1.m4a",
@@ -149,7 +167,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 124
+        "chapter": 124,
+        "id": "d02aadfd9a04431eb788ad9f90843646"
     },
     {
         "title": "125.《哈利·波特》第三部 第20集 衣柜里的博格特2.m4a",
@@ -157,7 +176,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 125
+        "chapter": 125,
+        "id": "a2f18782e5544141b9c710174a3daa02"
     },
     {
         "title": "126.《哈利·波特》第三部 第21集 衣柜里的博格特3.m4a",
@@ -165,7 +185,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 126
+        "chapter": 126,
+        "id": "e24ac4a731c244c9a89b0334dbbb5274"
     },
     {
         "title": "127.《哈利·波特》第三部 第22集 胖夫人逃跑1.m4a",
@@ -173,7 +194,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 127
+        "chapter": 127,
+        "id": "984ad0a6fbbe43098bebe28b06ac93ed"
     },
     {
         "title": "128.《哈利·波特》第三部 第23集 胖夫人逃跑2.m4a",
@@ -181,7 +203,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 128
+        "chapter": 128,
+        "id": "709f8f139fca4ebbb6070fa71425d20e"
     },
     {
         "title": "129.《哈利·波特》第三部 第24集 胖夫人逃跑3.m4a",
@@ -189,7 +212,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 129
+        "chapter": 129,
+        "id": "3b0ac57ba67c493c9489fa00e3abe791"
     },
     {
         "title": "130.《哈利·波特》第三部 第25集 不祥的失败1.m4a",
@@ -197,7 +221,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 130
+        "chapter": 130,
+        "id": "9a76fa91b7b14b959006577948ef974e"
     },
     {
         "title": "131.《哈利·波特》第三部 第26集 不祥的失败2.m4a",
@@ -205,7 +230,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 131
+        "chapter": 131,
+        "id": "053641f98f6e45698f6297a37c3a01e6"
     },
     {
         "title": "132.《哈利·波特》第三部 第27集 不祥的失败3.m4a",
@@ -213,7 +239,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 132
+        "chapter": 132,
+        "id": "db32f204d8b54c2e818d2a28efdbc988"
     },
     {
         "title": "133.《哈利·波特》第三部 第28集 活点地图1.m4a",
@@ -221,7 +248,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 133
+        "chapter": 133,
+        "id": "11d81e12847b4bddb43549f62cede5fb"
     },
     {
         "title": "134.《哈利·波特》第三部 第29集 活点地图2.m4a",
@@ -229,7 +257,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 134
+        "chapter": 134,
+        "id": "8c9e7d1efa334823a1ab96646aaee52d"
     },
     {
         "title": "135.《哈利·波特》第三部 第30集 活点地图3.m4a",
@@ -237,7 +266,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 135
+        "chapter": 135,
+        "id": "ffd939c8083243679d28282d280de92a"
     },
     {
         "title": "136.《哈利·波特》第三部 第31集 活点地图4.m4a",
@@ -245,7 +275,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 136
+        "chapter": 136,
+        "id": "4d7c452e72e648058af22a94a186910e"
     },
     {
         "title": "137.《哈利·波特》第三部 第32集 火弩箭1.m4a",
@@ -253,7 +284,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 137
+        "chapter": 137,
+        "id": "be7df50c67874301a16b8adf6db4e665"
     },
     {
         "title": "138.《哈利·波特》第三部 第33集 火弩箭2.m4a",
@@ -261,7 +293,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 138
+        "chapter": 138,
+        "id": "bab24c52344e405888934edda5530655"
     },
     {
         "title": "139.《哈利·波特》第三部 第34集 火弩箭3.m4a",
@@ -269,7 +302,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 139
+        "chapter": 139,
+        "id": "589d9848fa8042e4b9e4f738cf8c8353"
     },
     {
         "title": "140.《哈利·波特》第三部 第35集 守护神1.m4a",
@@ -277,7 +311,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 140
+        "chapter": 140,
+        "id": "ac5c553680c547ec8d183fda317e6e0b"
     },
     {
         "title": "141.《哈利·波特》第三部 第36集 守护神2.m4a",
@@ -285,7 +320,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 141
+        "chapter": 141,
+        "id": "7c6ab5c14ab24daf839f7bb70d1fc895"
     },
     {
         "title": "142.《哈利·波特》第三部 第37集 守护神3.m4a",
@@ -293,7 +329,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 142
+        "chapter": 142,
+        "id": "894623da4c0c47b68a61946ae9143365"
     },
     {
         "title": "143.《哈利·波特》第三部 第38集 格兰芬多对拉文克劳1.m4a",
@@ -301,7 +338,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 143
+        "chapter": 143,
+        "id": "6e727f51883c486e9a0a58aea2272f4f"
     },
     {
         "title": "144.《哈利·波特》第三部 第39集 格兰芬多对拉文克劳2.m4a",
@@ -309,7 +347,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 144
+        "chapter": 144,
+        "id": "11ff82ec9d9c4a9f9712f7325a575424"
     },
     {
         "title": "145.《哈利·波特》第三部 第40集 斯内普怀恨在心1.m4a",
@@ -317,7 +356,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 145
+        "chapter": 145,
+        "id": "db6a589774184300ab4a744fc470bb9d"
     },
     {
         "title": "146.《哈利·波特》第三部 第41集 斯内普怀恨在心2.m4a",
@@ -325,7 +365,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 146
+        "chapter": 146,
+        "id": "57ec87460eae4d278eec4e2a2b4a96ec"
     },
     {
         "title": "147.《哈利·波特》第三部 第42集 斯内普怀恨在心3.m4a",
@@ -333,7 +374,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 147
+        "chapter": 147,
+        "id": "63c9ad1cc58249c6964d1052f5546e6a"
     },
     {
         "title": "148.《哈利·波特》第三部 第43集 魁地奇决赛1.m4a",
@@ -341,7 +383,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 148
+        "chapter": 148,
+        "id": "6c7606f1108f4085b74f0fa28f9c1732"
     },
     {
         "title": "149.《哈利·波特》第三部 第44集 魁地奇决赛2.m4a",
@@ -349,7 +392,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 149
+        "chapter": 149,
+        "id": "66ed0c17ca194888b9891ab4548fdd43"
     },
     {
         "title": "150.《哈利·波特》第三部 第45集 魁地奇决赛3.m4a",
@@ -357,7 +401,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 150
+        "chapter": 150,
+        "id": "1a212730732f479ca962f3031c638ce8"
     },
     {
         "title": "151.《哈利·波特》第三部 第46集 特里劳尼教授的预言1.m4a",
@@ -365,7 +410,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 151
+        "chapter": 151,
+        "id": "e173224ffee74736a111c13e11c6f653"
     },
     {
         "title": "152.《哈利·波特》第三部 第47集 特里劳尼教授的预言2.m4a",
@@ -373,7 +419,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 152
+        "chapter": 152,
+        "id": "927ea7b238024dd883ab9283e4ee9df4"
     },
     {
         "title": "153.《哈利·波特》第三部 第48集 特里劳尼教授的预言3.m4a",
@@ -381,7 +428,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 153
+        "chapter": 153,
+        "id": "63f52ccea66e46b4a4aab2c45ba1bbaa"
     },
     {
         "title": "154.《哈利·波特》第三部 第49集 猫，老鼠和狗1.m4a",
@@ -389,7 +437,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 154
+        "chapter": 154,
+        "id": "8bf1307dffb5442c8b89a5fcc5db2077"
     },
     {
         "title": "155.《哈利·波特》第三部 第50集 猫，老鼠和狗2.m4a",
@@ -397,7 +446,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 155
+        "chapter": 155,
+        "id": "99c9ea6f527e46628f67e08c34d4f6c9"
     },
     {
         "title": "156.《哈利·波特》第三部 第51集 猫，老鼠和狗3.m4a",
@@ -405,7 +455,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 156
+        "chapter": 156,
+        "id": "151461a4e6034ad4ad0c6327fe97d529"
     },
     {
         "title": "157.《哈利·波特》第三部 第52集 月亮脸、虫尾巴、大脚板和尖头叉子.m4a",
@@ -413,7 +464,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 157
+        "chapter": 157,
+        "id": "328c297000d04699ad43b5f57507cc9b"
     },
     {
         "title": "158.《哈利·波特》第三部 第53集 伏地魔的仆人1.m4a",
@@ -421,7 +473,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 158
+        "chapter": 158,
+        "id": "3bb6272cfda04a46b650cb4539f8e8c0"
     },
     {
         "title": "159.《哈利·波特》第三部 第54集 伏地魔的仆人2.m4a",
@@ -429,7 +482,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 159
+        "chapter": 159,
+        "id": "beb14d32acb742baafffad072bc8b81d"
     },
     {
         "title": "160.《哈利·波特》第三部 第55集 伏地魔的仆人3.m4a",
@@ -437,7 +491,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 160
+        "chapter": 160,
+        "id": "56ce236d8a794be1a53eac36973350e3"
     },
     {
         "title": "161.《哈利·波特》第三部 第56集 摄魂怪的吻.m4a",
@@ -445,7 +500,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 161
+        "chapter": 161,
+        "id": "c91610fa4fdc421dbfeecdc7184480e3"
     },
     {
         "title": "162.《哈利·波特》第三部 第57集 赫敏的秘密1.m4a",
@@ -453,7 +509,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 162
+        "chapter": 162,
+        "id": "30b012a113ab496487489be898402f5e"
     },
     {
         "title": "163.《哈利·波特》第三部 第58集 赫敏的秘密2.m4a",
@@ -461,7 +518,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 163
+        "chapter": 163,
+        "id": "d6d8d1ab4e1f4660889a1c0464c6ce58"
     },
     {
         "title": "164.《哈利·波特》第三部 第59集 赫敏的秘密3.m4a",
@@ -469,7 +527,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 164
+        "chapter": 164,
+        "id": "4f0611d48a2f4727a0cc9b5bc3e383b1"
     },
     {
         "title": "165.《哈利·波特》第三部 第60集 赫敏的秘密4.m4a",
@@ -477,7 +536,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 165
+        "chapter": 165,
+        "id": "5e7f02dbe67b402f982c42c51d3f0304"
     },
     {
         "title": "166.《哈利·波特》第三部 第61集 又见猫头鹰传书1.m4a",
@@ -485,7 +545,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 166
+        "chapter": 166,
+        "id": "fbee8e27d1f642408aa15f98c9c87efc"
     },
     {
         "title": "167.《哈利·波特》第三部 第62集 又见猫头鹰传书2.m4a",
@@ -493,7 +554,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 167
+        "chapter": 167,
+        "id": "873f402e1c0e4d4fb56963714fdd0ca0"
     },
     {
         "title": "168.《哈利·波特》第三部 第63集 又见猫头鹰传书3.m4a",
@@ -501,7 +563,8 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 168
+        "chapter": 168,
+        "id": "84419a4e9a2a4a0cb5db604449a3e310"
     },
     {
         "title": "169.【片花】魔法铁三角.m4a",
@@ -509,6 +572,7 @@
         "duration": "",
         "published": "2024-05-21",
         "createTime": "",
-        "chapter": 169
+        "chapter": 169,
+        "id": "283fff99433345a69d9de44b3bf562bb"
     }
 ]
