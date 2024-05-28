0d6b74882d6d60dc3f3372111f67c4f20d85d49b
xiongod <xiongod@outlook.com>
2024-05-28 17:11:35 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 [
     {
-        "title": "106.【试听】《哈利·波特》第三部 第1集 猫头鹰传书1.m4a",
+        "title": "【试听】《哈利·波特》第三部 第集 猫头鹰传书.ma",
         "url": "https://xiongod.github.io/halibote3/106.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -9,7 +9,7 @@
         "id": "674adad9155540a098c3685510ce5462"
     },
     {
-        "title": "107.【试听】《哈利·波特》第三部 第2集 猫头鹰传书2.m4a",
+        "title": "【试听】《哈利·波特》第三部 第集 猫头鹰传书.ma",
         "url": "https://xiongod.github.io/halibote3/107.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -18,7 +18,7 @@
         "id": "b3264b5fb66a41b9b6f4c73ae2b5944b"
     },
     {
-        "title": "108.【试听】《哈利·波特》第三部 第3集 玛姬姑妈的大错误1.m4a",
+        "title": "【试听】《哈利·波特》第三部 第集 玛姬姑妈的大错误.ma",
         "url": "https://xiongod.github.io/halibote3/108.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -27,7 +27,7 @@
         "id": "912974d702a74dc3a0f8f721176745a6"
     },
     {
-        "title": "109.《哈利·波特》第三部 第4集 玛姬姑妈的大错误2.m4a",
+        "title": "《哈利·波特》第三部 第集 玛姬姑妈的大错误.ma",
         "url": "https://xiongod.github.io/halibote3/109.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -36,7 +36,7 @@
         "id": "3b4d44a508774f3699fc25cc98e25b23"
     },
     {
-        "title": "110.《哈利·波特》第三部 第5集 骑士公共汽车1.m4a",
+        "title": "《哈利·波特》第三部 第集 骑士公共汽车.ma",
         "url": "https://xiongod.github.io/halibote3/110.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -45,7 +45,7 @@
         "id": "1c40b43374d24f23a3702aae86958159"
     },
     {
-        "title": "111.《哈利·波特》第三部 第6集 骑士公共汽车2.m4a",
+        "title": "《哈利·波特》第三部 第集 骑士公共汽车.ma",
         "url": "https://xiongod.github.io/halibote3/111.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -54,7 +54,7 @@
         "id": "7a01ef752e27477c8e6fb274c2137f84"
     },
     {
-        "title": "112.《哈利·波特》第三部 第7集 破釜酒吧1.m4a",
+        "title": "《哈利·波特》第三部 第集 破釜酒吧.ma",
         "url": "https://xiongod.github.io/halibote3/112.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -63,7 +63,7 @@
         "id": "7caac464e6694bf8be0afacdc5a81aab"
     },
     {
-        "title": "113.《哈利·波特》第三部 第8集 破釜酒吧2.m4a",
+        "title": "《哈利·波特》第三部 第集 破釜酒吧.ma",
         "url": "https://xiongod.github.io/halibote3/113.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -72,7 +72,7 @@
         "id": "3fb1db98210249c4821c1c8aba92fe89"
     },
     {
-        "title": "114.《哈利·波特》第三部 第9集 破釜酒吧3.m4a",
+        "title": "《哈利·波特》第三部 第集 破釜酒吧.ma",
         "url": "https://xiongod.github.io/halibote3/114.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -81,7 +81,7 @@
         "id": "7f092f0b508e49a88699e5e43ca0b811"
     },
     {
-        "title": "115.《哈利·波特》第三部 第10集 摄魂怪1.m4a",
+        "title": "《哈利·波特》第三部 第集 摄魂怪.ma",
         "url": "https://xiongod.github.io/halibote3/115.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -90,7 +90,7 @@
         "id": "2151e353c6e94215bc0e43555d557b86"
     },
     {
-        "title": "116.《哈利·波特》第三部 第11集 摄魂怪2.m4a",
+        "title": "《哈利·波特》第三部 第集 摄魂怪.ma",
         "url": "https://xiongod.github.io/halibote3/116.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -99,7 +99,7 @@
         "id": "dccc60f7bb6143f890f39dcd00c281a6"
     },
     {
-        "title": "117.《哈利·波特》第三部 第12集 摄魂怪3.m4a",
+        "title": "《哈利·波特》第三部 第集 摄魂怪.ma",
         "url": "https://xiongod.github.io/halibote3/117.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -108,7 +108,7 @@
         "id": "9205c0d576094732aa8884471afdd408"
     },
     {
-        "title": "118.《哈利·波特》第三部 第13集 摄魂怪4.m4a",
+        "title": "《哈利·波特》第三部 第集 摄魂怪.ma",
         "url": "https://xiongod.github.io/halibote3/118.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -117,7 +117,7 @@
         "id": "5262e26a5f1f452793e68194d6edf67b"
     },
     {
-        "title": "119.《哈利·波特》第三部 第14集 鹰爪和茶叶1.m4a",
+        "title": "《哈利·波特》第三部 第集 鹰爪和茶叶.ma",
         "url": "https://xiongod.github.io/halibote3/119.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -126,7 +126,7 @@
         "id": "fcc3412e45024eb784833d14ab7f03eb"
     },
     {
-        "title": "120.《哈利·波特》第三部 第15集 鹰爪和茶叶2.m4a",
+        "title": "《哈利·波特》第三部 第集 鹰爪和茶叶.ma",
         "url": "https://xiongod.github.io/halibote3/120.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -135,7 +135,7 @@
         "id": "11e6294878144661b677cd1d0cf81f9f"
     },
     {
-        "title": "121.《哈利·波特》第三部 第16集 鹰爪和茶叶3.m4a",
+        "title": "《哈利·波特》第三部 第集 鹰爪和茶叶.ma",
         "url": "https://xiongod.github.io/halibote3/121.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -144,7 +144,7 @@
         "id": "13a69f88a27e4838b2790a82e56488a3"
     },
     {
-        "title": "122.《哈利·波特》第三部 第17集 鹰爪和茶叶4.m4a",
+        "title": "《哈利·波特》第三部 第集 鹰爪和茶叶.ma",
         "url": "https://xiongod.github.io/halibote3/122.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -153,7 +153,7 @@
         "id": "d80b5ce9a9e94a14b0079b45ddf4298b"
     },
     {
-        "title": "123.《哈利·波特》第三部 第18集 鹰爪和茶叶5.m4a",
+        "title": "《哈利·波特》第三部 第集 鹰爪和茶叶.ma",
         "url": "https://xiongod.github.io/halibote3/123.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -162,7 +162,7 @@
         "id": "8b897824d3654da2af48648f599de2c4"
     },
     {
-        "title": "124.《哈利·波特》第三部 第19集 衣柜里的博格特1.m4a",
+        "title": "《哈利·波特》第三部 第集 衣柜里的博格特.ma",
         "url": "https://xiongod.github.io/halibote3/124.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -171,7 +171,7 @@
         "id": "d02aadfd9a04431eb788ad9f90843646"
     },
     {
-        "title": "125.《哈利·波特》第三部 第20集 衣柜里的博格特2.m4a",
+        "title": "《哈利·波特》第三部 第集 衣柜里的博格特.ma",
         "url": "https://xiongod.github.io/halibote3/125.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -180,7 +180,7 @@
         "id": "a2f18782e5544141b9c710174a3daa02"
     },
     {
-        "title": "126.《哈利·波特》第三部 第21集 衣柜里的博格特3.m4a",
+        "title": "《哈利·波特》第三部 第集 衣柜里的博格特.ma",
         "url": "https://xiongod.github.io/halibote3/126.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -189,7 +189,7 @@
         "id": "e24ac4a731c244c9a89b0334dbbb5274"
     },
     {
-        "title": "127.《哈利·波特》第三部 第22集 胖夫人逃跑1.m4a",
+        "title": "《哈利·波特》第三部 第集 胖夫人逃跑.ma",
         "url": "https://xiongod.github.io/halibote3/127.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -198,7 +198,7 @@
         "id": "984ad0a6fbbe43098bebe28b06ac93ed"
     },
     {
-        "title": "128.《哈利·波特》第三部 第23集 胖夫人逃跑2.m4a",
+        "title": "《哈利·波特》第三部 第集 胖夫人逃跑.ma",
         "url": "https://xiongod.github.io/halibote3/128.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -207,7 +207,7 @@
         "id": "709f8f139fca4ebbb6070fa71425d20e"
     },
     {
-        "title": "129.《哈利·波特》第三部 第24集 胖夫人逃跑3.m4a",
+        "title": "《哈利·波特》第三部 第集 胖夫人逃跑.ma",
         "url": "https://xiongod.github.io/halibote3/129.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -216,7 +216,7 @@
         "id": "3b0ac57ba67c493c9489fa00e3abe791"
     },
     {
-        "title": "130.《哈利·波特》第三部 第25集 不祥的失败1.m4a",
+        "title": "《哈利·波特》第三部 第集 不祥的失败.ma",
         "url": "https://xiongod.github.io/halibote3/130.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -225,7 +225,7 @@
         "id": "9a76fa91b7b14b959006577948ef974e"
     },
     {
-        "title": "131.《哈利·波特》第三部 第26集 不祥的失败2.m4a",
+        "title": "《哈利·波特》第三部 第集 不祥的失败.ma",
         "url": "https://xiongod.github.io/halibote3/131.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -234,7 +234,7 @@
         "id": "053641f98f6e45698f6297a37c3a01e6"
     },
     {
-        "title": "132.《哈利·波特》第三部 第27集 不祥的失败3.m4a",
+        "title": "《哈利·波特》第三部 第集 不祥的失败.ma",
         "url": "https://xiongod.github.io/halibote3/132.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -243,7 +243,7 @@
         "id": "db32f204d8b54c2e818d2a28efdbc988"
     },
     {
-        "title": "133.《哈利·波特》第三部 第28集 活点地图1.m4a",
+        "title": "《哈利·波特》第三部 第集 活点地图.ma",
         "url": "https://xiongod.github.io/halibote3/133.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -252,7 +252,7 @@
         "id": "11d81e12847b4bddb43549f62cede5fb"
     },
     {
-        "title": "134.《哈利·波特》第三部 第29集 活点地图2.m4a",
+        "title": "《哈利·波特》第三部 第集 活点地图.ma",
         "url": "https://xiongod.github.io/halibote3/134.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -261,7 +261,7 @@
         "id": "8c9e7d1efa334823a1ab96646aaee52d"
     },
     {
-        "title": "135.《哈利·波特》第三部 第30集 活点地图3.m4a",
+        "title": "《哈利·波特》第三部 第集 活点地图.ma",
         "url": "https://xiongod.github.io/halibote3/135.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -270,7 +270,7 @@
         "id": "ffd939c8083243679d28282d280de92a"
     },
     {
-        "title": "136.《哈利·波特》第三部 第31集 活点地图4.m4a",
+        "title": "《哈利·波特》第三部 第集 活点地图.ma",
         "url": "https://xiongod.github.io/halibote3/136.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -279,7 +279,7 @@
         "id": "4d7c452e72e648058af22a94a186910e"
     },
     {
-        "title": "137.《哈利·波特》第三部 第32集 火弩箭1.m4a",
+        "title": "《哈利·波特》第三部 第集 火弩箭.ma",
         "url": "https://xiongod.github.io/halibote3/137.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -288,7 +288,7 @@
         "id": "be7df50c67874301a16b8adf6db4e665"
     },
     {
-        "title": "138.《哈利·波特》第三部 第33集 火弩箭2.m4a",
+        "title": "《哈利·波特》第三部 第集 火弩箭.ma",
         "url": "https://xiongod.github.io/halibote3/138.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -297,7 +297,7 @@
         "id": "bab24c52344e405888934edda5530655"
     },
     {
-        "title": "139.《哈利·波特》第三部 第34集 火弩箭3.m4a",
+        "title": "《哈利·波特》第三部 第集 火弩箭.ma",
         "url": "https://xiongod.github.io/halibote3/139.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -306,7 +306,7 @@
         "id": "589d9848fa8042e4b9e4f738cf8c8353"
     },
     {
-        "title": "140.《哈利·波特》第三部 第35集 守护神1.m4a",
+        "title": "《哈利·波特》第三部 第集 守护神.ma",
         "url": "https://xiongod.github.io/halibote3/140.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -315,7 +315,7 @@
         "id": "ac5c553680c547ec8d183fda317e6e0b"
     },
     {
-        "title": "141.《哈利·波特》第三部 第36集 守护神2.m4a",
+        "title": "《哈利·波特》第三部 第集 守护神.ma",
         "url": "https://xiongod.github.io/halibote3/141.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -324,7 +324,7 @@
         "id": "7c6ab5c14ab24daf839f7bb70d1fc895"
     },
     {
-        "title": "142.《哈利·波特》第三部 第37集 守护神3.m4a",
+        "title": "《哈利·波特》第三部 第集 守护神.ma",
         "url": "https://xiongod.github.io/halibote3/142.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -333,7 +333,7 @@
         "id": "894623da4c0c47b68a61946ae9143365"
     },
     {
-        "title": "143.《哈利·波特》第三部 第38集 格兰芬多对拉文克劳1.m4a",
+        "title": "《哈利·波特》第三部 第集 格兰芬多对拉文克劳.ma",
         "url": "https://xiongod.github.io/halibote3/143.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -342,7 +342,7 @@
         "id": "6e727f51883c486e9a0a58aea2272f4f"
     },
     {
-        "title": "144.《哈利·波特》第三部 第39集 格兰芬多对拉文克劳2.m4a",
+        "title": "《哈利·波特》第三部 第集 格兰芬多对拉文克劳.ma",
         "url": "https://xiongod.github.io/halibote3/144.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -351,7 +351,7 @@
         "id": "11ff82ec9d9c4a9f9712f7325a575424"
     },
     {
-        "title": "145.《哈利·波特》第三部 第40集 斯内普怀恨在心1.m4a",
+        "title": "《哈利·波特》第三部 第集 斯内普怀恨在心.ma",
         "url": "https://xiongod.github.io/halibote3/145.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -360,7 +360,7 @@
         "id": "db6a589774184300ab4a744fc470bb9d"
     },
     {
-        "title": "146.《哈利·波特》第三部 第41集 斯内普怀恨在心2.m4a",
+        "title": "《哈利·波特》第三部 第集 斯内普怀恨在心.ma",
         "url": "https://xiongod.github.io/halibote3/146.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -369,7 +369,7 @@
         "id": "57ec87460eae4d278eec4e2a2b4a96ec"
     },
     {
-        "title": "147.《哈利·波特》第三部 第42集 斯内普怀恨在心3.m4a",
+        "title": "《哈利·波特》第三部 第集 斯内普怀恨在心.ma",
         "url": "https://xiongod.github.io/halibote3/147.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -378,7 +378,7 @@
         "id": "63c9ad1cc58249c6964d1052f5546e6a"
     },
     {
-        "title": "148.《哈利·波特》第三部 第43集 魁地奇决赛1.m4a",
+        "title": "《哈利·波特》第三部 第集 魁地奇决赛.ma",
         "url": "https://xiongod.github.io/halibote3/148.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -387,7 +387,7 @@
         "id": "6c7606f1108f4085b74f0fa28f9c1732"
     },
     {
-        "title": "149.《哈利·波特》第三部 第44集 魁地奇决赛2.m4a",
+        "title": "《哈利·波特》第三部 第集 魁地奇决赛.ma",
         "url": "https://xiongod.github.io/halibote3/149.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -396,7 +396,7 @@
         "id": "66ed0c17ca194888b9891ab4548fdd43"
     },
     {
-        "title": "150.《哈利·波特》第三部 第45集 魁地奇决赛3.m4a",
+        "title": "《哈利·波特》第三部 第集 魁地奇决赛.ma",
         "url": "https://xiongod.github.io/halibote3/150.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -405,7 +405,7 @@
         "id": "1a212730732f479ca962f3031c638ce8"
     },
     {
-        "title": "151.《哈利·波特》第三部 第46集 特里劳尼教授的预言1.m4a",
+        "title": "《哈利·波特》第三部 第集 特里劳尼教授的预言.ma",
         "url": "https://xiongod.github.io/halibote3/151.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -414,7 +414,7 @@
         "id": "e173224ffee74736a111c13e11c6f653"
     },
     {
-        "title": "152.《哈利·波特》第三部 第47集 特里劳尼教授的预言2.m4a",
+        "title": "《哈利·波特》第三部 第集 特里劳尼教授的预言.ma",
         "url": "https://xiongod.github.io/halibote3/152.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -423,7 +423,7 @@
         "id": "927ea7b238024dd883ab9283e4ee9df4"
     },
     {
-        "title": "153.《哈利·波特》第三部 第48集 特里劳尼教授的预言3.m4a",
+        "title": "《哈利·波特》第三部 第集 特里劳尼教授的预言.ma",
         "url": "https://xiongod.github.io/halibote3/153.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -432,7 +432,7 @@
         "id": "63f52ccea66e46b4a4aab2c45ba1bbaa"
     },
     {
-        "title": "154.《哈利·波特》第三部 第49集 猫，老鼠和狗1.m4a",
+        "title": "《哈利·波特》第三部 第集 猫，老鼠和狗.ma",
         "url": "https://xiongod.github.io/halibote3/154.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -441,7 +441,7 @@
         "id": "8bf1307dffb5442c8b89a5fcc5db2077"
     },
     {
-        "title": "155.《哈利·波特》第三部 第50集 猫，老鼠和狗2.m4a",
+        "title": "《哈利·波特》第三部 第集 猫，老鼠和狗.ma",
         "url": "https://xiongod.github.io/halibote3/155.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -450,7 +450,7 @@
         "id": "99c9ea6f527e46628f67e08c34d4f6c9"
     },
     {
-        "title": "156.《哈利·波特》第三部 第51集 猫，老鼠和狗3.m4a",
+        "title": "《哈利·波特》第三部 第集 猫，老鼠和狗.ma",
         "url": "https://xiongod.github.io/halibote3/156.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -459,7 +459,7 @@
         "id": "151461a4e6034ad4ad0c6327fe97d529"
     },
     {
-        "title": "157.《哈利·波特》第三部 第52集 月亮脸、虫尾巴、大脚板和尖头叉子.m4a",
+        "title": "《哈利·波特》第三部 第集 月亮脸、虫尾巴、大脚板和尖头叉子.ma",
         "url": "https://xiongod.github.io/halibote3/157.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -468,7 +468,7 @@
         "id": "328c297000d04699ad43b5f57507cc9b"
     },
     {
-        "title": "158.《哈利·波特》第三部 第53集 伏地魔的仆人1.m4a",
+        "title": "《哈利·波特》第三部 第集 伏地魔的仆人.ma",
         "url": "https://xiongod.github.io/halibote3/158.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -477,7 +477,7 @@
         "id": "3bb6272cfda04a46b650cb4539f8e8c0"
     },
     {
-        "title": "159.《哈利·波特》第三部 第54集 伏地魔的仆人2.m4a",
+        "title": "《哈利·波特》第三部 第集 伏地魔的仆人.ma",
         "url": "https://xiongod.github.io/halibote3/159.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -486,7 +486,7 @@
         "id": "beb14d32acb742baafffad072bc8b81d"
     },
     {
-        "title": "160.《哈利·波特》第三部 第55集 伏地魔的仆人3.m4a",
+        "title": "《哈利·波特》第三部 第集 伏地魔的仆人.ma",
         "url": "https://xiongod.github.io/halibote3/160.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -495,7 +495,7 @@
         "id": "56ce236d8a794be1a53eac36973350e3"
     },
     {
-        "title": "161.《哈利·波特》第三部 第56集 摄魂怪的吻.m4a",
+        "title": "《哈利·波特》第三部 第集 摄魂怪的吻.ma",
         "url": "https://xiongod.github.io/halibote3/161.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -504,7 +504,7 @@
         "id": "c91610fa4fdc421dbfeecdc7184480e3"
     },
     {
-        "title": "162.《哈利·波特》第三部 第57集 赫敏的秘密1.m4a",
+        "title": "《哈利·波特》第三部 第集 赫敏的秘密.ma",
         "url": "https://xiongod.github.io/halibote3/162.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -513,7 +513,7 @@
         "id": "30b012a113ab496487489be898402f5e"
     },
     {
-        "title": "163.《哈利·波特》第三部 第58集 赫敏的秘密2.m4a",
+        "title": "《哈利·波特》第三部 第集 赫敏的秘密.ma",
         "url": "https://xiongod.github.io/halibote3/163.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -522,7 +522,7 @@
         "id": "d6d8d1ab4e1f4660889a1c0464c6ce58"
     },
     {
-        "title": "164.《哈利·波特》第三部 第59集 赫敏的秘密3.m4a",
+        "title": "《哈利·波特》第三部 第集 赫敏的秘密.ma",
         "url": "https://xiongod.github.io/halibote3/164.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -531,7 +531,7 @@
         "id": "4f0611d48a2f4727a0cc9b5bc3e383b1"
     },
     {
-        "title": "165.《哈利·波特》第三部 第60集 赫敏的秘密4.m4a",
+        "title": "《哈利·波特》第三部 第集 赫敏的秘密.ma",
         "url": "https://xiongod.github.io/halibote3/165.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -540,7 +540,7 @@
         "id": "5e7f02dbe67b402f982c42c51d3f0304"
     },
     {
-        "title": "166.《哈利·波特》第三部 第61集 又见猫头鹰传书1.m4a",
+        "title": "《哈利·波特》第三部 第集 又见猫头鹰传书.ma",
         "url": "https://xiongod.github.io/halibote3/166.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -549,7 +549,7 @@
         "id": "fbee8e27d1f642408aa15f98c9c87efc"
     },
     {
-        "title": "167.《哈利·波特》第三部 第62集 又见猫头鹰传书2.m4a",
+        "title": "《哈利·波特》第三部 第集 又见猫头鹰传书.ma",
         "url": "https://xiongod.github.io/halibote3/167.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -558,7 +558,7 @@
         "id": "873f402e1c0e4d4fb56963714fdd0ca0"
     },
     {
-        "title": "168.《哈利·波特》第三部 第63集 又见猫头鹰传书3.m4a",
+        "title": "《哈利·波特》第三部 第集 又见猫头鹰传书.ma",
         "url": "https://xiongod.github.io/halibote3/168.m4a",
         "duration": "",
         "published": "2024-05-21",
@@ -567,7 +567,7 @@
         "id": "84419a4e9a2a4a0cb5db604449a3e310"
     },
     {
-        "title": "169.【片花】魔法铁三角.m4a",
+        "title": "【片花】魔法铁三角.ma",
         "url": "https://xiongod.github.io/halibote3/169.m4a",
         "duration": "",
         "published": "2024-05-21",
